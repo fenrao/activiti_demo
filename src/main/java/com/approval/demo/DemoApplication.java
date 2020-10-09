ece8ac14887f574f49fb1d82d934993f654c565c
fuzengbin <123qweasd>
2020-10-09 10:38:53 +0800

    web页面整合activiti-modeler可视化设计器

--- src/main/java/com/approval/demo/DemoApplication.java
+++ src/main/java/com/approval/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.approval.demo;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//禁用activiti中自动集成的security的权限验证，当我们访问接口的时候会弹出登录界面，所以我们需要禁用掉登录验证在启动类中添加注解。
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+                                    org.activiti.spring.boot.SecurityAutoConfiguration.class})
 public class DemoApplication {
 
     public static void main(String[] args) {
